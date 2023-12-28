7c3809956edbf5d057a8d8c31bec90d67ec913f8
olgacode <olgabarlou@gmail.com>
2023-12-29 00:05:46 +0200

    add scripts

--- scripts/eval_gsm.sh
+++ scripts/eval_gsm.sh
@@ -1,8 +1,8 @@
 python src/analyze.py \
-  --plan outputs/gsm/plan/demo_plan_0_subset.jsonl \
-  --cot outputs/gsm/cot/demo_cot_0_subset.jsonl \
-  --pot outputs/gsm/pot/demo_pot_0_subset.jsonl \
-  --eot outputs/gsm/eot/demo_eot_0_subset.jsonl \
-  --pot_assertion outputs/gsm/check_pot/demo_check_pot_0_subset.jsonl \
-  --eot_assertion outputs/gsm/check_eot/demo_check_eot_0_subset.jsonl \
+  --plan outputs/gsm/plan/demo_plan_0_end.jsonl \
+  --cot outputs/gsm/cot/demo_cot_0_end.jsonl \
+  --pot outputs/gsm/pot/demo_pot_0_end.jsonl \
+  --eot outputs/gsm/eot/demo_eot_0_end.jsonl \
+  --pot_assertion outputs/gsm/check_pot/demo_check_pot_0_end.jsonl \
+  --eot_assertion outputs/gsm/check_eot/demo_check_eot_0_end.jsonl \
   --tag gsm_analysis
