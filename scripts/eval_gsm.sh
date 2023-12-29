eede05b840865587be8e50d49e48bd8731a6f1c9
olgacode <olgabarlou@gmail.com>
2023-12-29 18:10:53 +0200

    debugging

--- scripts/eval_gsm.sh
+++ scripts/eval_gsm.sh
@@ -1,11 +1,12 @@
-RANGE_START=0
-RANGE_END=1
+RANGE_START=$1
+RANGE_END=$2
+TAG=$3
 
 python src/analyze.py \
-  --plan outputs/gsm/plan/demo_plan_${RANGE_START}_${RANGE_END}.jsonl \
-  --cot outputs/gsm/cot/demo_cot_${RANGE_START}_${RANGE_END}.jsonl \
-  --pot outputs/gsm/pot/demo_pot_${RANGE_START}_${RANGE_END}.jsonl \
-  --eot outputs/gsm/eot/demo_eot_${RANGE_START}_${RANGE_END}.jsonl \
-  --pot_assertion outputs/gsm/check_pot/demo_check_pot_${RANGE_START}_${RANGE_END}.jsonl \
-  --eot_assertion outputs/gsm/check_eot/demo_check_eot_${RANGE_START}_${RANGE_END}.jsonl \
+  --plan outputs/gsm/plan/${TAG}_plan_${RANGE_START}_${RANGE_END}.jsonl \
+  --cot outputs/gsm/cot/${TAG}_cot_${RANGE_START}_${RANGE_END}.jsonl \
+  --pot outputs/gsm/pot/${TAG}_pot_${RANGE_START}_${RANGE_END}.jsonl \
+  --eot outputs/gsm/eot/${TAG}_eot_${RANGE_START}_${RANGE_END}.jsonl \
+  --pot_assertion outputs/gsm/check_pot/${TAG}_check_pot_${RANGE_START}_${RANGE_END}.jsonl \
+  --eot_assertion outputs/gsm/check_eot/${TAG}_check_eot_${RANGE_START}_${RANGE_END}.jsonl \
   --tag gsm_analysis
