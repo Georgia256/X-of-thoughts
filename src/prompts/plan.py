7a8a6df8d413aa53099298613819ff075450e6ee
olgacode <olgabarlou@gmail.com>
2023-12-30 12:42:10 +0200

    debugging

--- src/prompts/plan.py
+++ src/prompts/plan.py
@@ -1,7 +1,6 @@
-# PLAN_SYSTEM = """
-# You are an expert in math reasoning. Please choose the best method to solve the math word problems. Read the description of the methods and the examples. You only need to suggest the best method. Do not solve the problem.
-# """.strip()
-PLAN_SYSTEM = """You are an expert in math reasoning. Please choose the best method to solve the math word problems. Do not solve the problem."""
+PLAN_SYSTEM = """
+You are an expert in math reasoning. Please choose the best method to solve the math word problems. Read the description of the methods and the examples. You only need to suggest the best method. Do not solve the problem.
+""".strip()
 
 # 8-shot
 PLAN = (
@@ -15,37 +14,17 @@ The methods are described as follows:
 
 Below are some examples that choose the most appropriate method for the math word problems.
 
-
-
 Question: Arnel had ten boxes of pencils with the same number of pencils in each box.  He kept ten pencils and shared the remaining pencils equally with his five friends. If his friends got eight pencils each, how many pencils are in each box?
-
 Method: System of linear equations
-
 
 Question: Larry spends half an hour twice a day walking and playing with his dog. He also spends a fifth of an hour every day feeding his dog. How many minutes does Larry spend on his dog each day?
-
 Method: Python Program
-
-
-Question: Angela is a bike messenger in New York. She needs to deliver 8 times as many packages as meals. If she needs to deliver 27 meals and packages combined, how many meals does she deliver?
-
-Method: System of linear equations
-
-
-Question: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
-
-Method: Python Program
-
 
 Question: Alyssa, Keely, and Kendall ordered 100 chicken nuggets from a fast-food restaurant. Keely and Kendall each ate twice as many as Alyssa. How many did Alyssa eat?
-
 Method: System of linear equations
 
-
 Question: Conor can chop 12 eggplants, 9 carrots, and 8 potatoes in a day. If he works 4 times a week, how many vegetables can he chop?
-
 Method: Python Program
-
 
 Question: {question}
 
