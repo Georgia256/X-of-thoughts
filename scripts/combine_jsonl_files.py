780700dd8828fc558571e390626b0257ce6f5269
olgacode <olgabarlou@gmail.com>
2024-01-08 15:28:00 +0200

    total results for GSM

--- scripts/combine_jsonl_files.py
+++ scripts/combine_jsonl_files.py
@@ -1,10 +1,15 @@
 import os
 import json
 import re
+import sys
 
 
-directory = "/Users/olga/Desktop/Pattern Recognition/Project/XoT-Pattern_Recognition_Project/outputs/gsm/plan"
-output_file = "/Users/olga/Desktop/Pattern Recognition/Project/XoT-Pattern_Recognition_Project/outputs/gsm/plan/phi_plan_0_end.jsonl"
+if len(sys.argv) != 3:
+    print("Usage: script.py <input_folder> <output_file>")
+    sys.exit(1)
+
+input_directory = sys.argv[1]
+output_file = sys.argv[2]
 
 
 # Function to extract number from filename
@@ -15,7 +20,9 @@ def extract_number(filename):
 
 # Collect all relevant files
 jsonl_files = [
-    f for f in os.listdir(directory) if f.startswith("batch") and f.endswith(".jsonl")
+    f
+    for f in os.listdir(input_directory)
+    if f.startswith("batch") and f.endswith(".jsonl")
 ]
 
 # Sort files based on the extracted number
@@ -24,6 +31,6 @@ jsonl_files.sort(key=extract_number)
 # Write sorted files to output
 with open(output_file, "w") as outfile:
     for filename in jsonl_files:
-        with open(os.path.join(directory, filename), "r") as infile:
+        with open(os.path.join(input_directory, filename), "r") as infile:
             for line in infile:
                 outfile.write(line)
