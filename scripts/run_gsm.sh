2796e9d8ee6b05efae0f65292a1c814d68c5ebf9
marilena1123 <mzoumpoulidi@gmail.com>
2024-01-09 13:00:15 +0200

    metacognitvie_eval add

--- scripts/run_gsm.sh
+++ scripts/run_gsm.sh
@@ -4,7 +4,7 @@ RANGE_START=$1
 RANGE_END=$2
 TAG=$3
 
-
+'''
 # cot
 python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode cot
 # eot
@@ -17,3 +17,7 @@ python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANG
 python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode check_pot --data_path outputs/gsm/pot/${TAG}_pot_${RANGE_START}_${RANGE_END}.jsonl
 # plan
 python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode plan
+'''
+#meta_eval
+python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode metacognitive_eval --data_path outputs/gsm/eot/${TAG}_eot_${RANGE_START}_${RANGE_END}.jsonl
+
