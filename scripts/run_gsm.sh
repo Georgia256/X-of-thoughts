eacc6344a387bf62c3ac2681c329915f8a66da70
olgacode <olgabarlou@gmail.com>
2024-01-12 13:29:56 +0200

    Update run_gsm.sh

--- scripts/run_gsm.sh
+++ scripts/run_gsm.sh
@@ -4,20 +4,20 @@ RANGE_START=$1
 RANGE_END=$2
 TAG=$3
 
-'''
+
 # cot
-python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode cot
+#python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode cot
 # eot
-python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode eot
+#python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode eot
 # check_eot
-python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode check_eot --data_path outputs/gsm/eot/${TAG}_eot_${RANGE_START}_${RANGE_END}.jsonl
+#python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode check_eot --data_path outputs/gsm/eot/${TAG}_eot_${RANGE_START}_${RANGE_END}.jsonl
 # pot
-python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode pot
+#python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode pot
 # check_pot
-python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode check_pot --data_path outputs/gsm/pot/${TAG}_pot_${RANGE_START}_${RANGE_END}.jsonl
+#python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode check_pot --data_path outputs/gsm/pot/${TAG}_pot_${RANGE_START}_${RANGE_END}.jsonl
 # plan
-python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode plan
-'''
+#python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode plan
+
 #meta_eval
 python src/solve.py --tag ${TAG} --range_start ${RANGE_START} --range_end ${RANGE_END} --dataset ${DATASET} --model ${MODEL} --mode metacognitive_eval_cot --data_path outputs/gsm/cot/${TAG}_cot_${RANGE_START}_${RANGE_END}.jsonl
 
