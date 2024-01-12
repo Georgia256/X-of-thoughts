95081b140808ca8d031632db9c2bfd757f8822ac
olgacode <olgabarlou@gmail.com>
2024-01-13 01:28:29 +0200

    Update metacognitive_eval_deepseek.py

--- src/prompts/metacognitive_eval_deepseek.py
+++ src/prompts/metacognitive_eval_deepseek.py
@@ -1,5 +1,5 @@
 META_EVAL_SYSTEM = """
-You are an expert in math reasoning. You will be given a problem, and a solution which you have to assess. 
+You are an expert in math reasoning. Assess the following solution.
 During the evaluation, perform the following: 
 1)Ensure that the problem's conditions are taken into account.
 2)Assess the appropriateness of the methods and formulas used.
@@ -11,10 +11,10 @@ After the evaluation, provide the following:
 -Provide the final numeric result seperately.
 
 Your answer must be in the following format:
-Evaluation:...
-Conclusion:...
-Correct solution:...
-Final numeric result:...
+Evaluation:
+Conclusion:
+Correct solution:
+Final numeric result:
 """.strip()
 
 META_EVAL = """
