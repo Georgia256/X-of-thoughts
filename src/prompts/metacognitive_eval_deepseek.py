77062198463a82e596acb07f52cabbfdd5494257
marilena1123 <mzoumpoulidi@gmail.com>
2024-01-15 15:38:29 +0200

    2nd option, metacognitive prompt

--- src/prompts/metacognitive_eval_deepseek.py
+++ src/prompts/metacognitive_eval_deepseek.py
@@ -1,3 +1,5 @@
+#1st option
+'''
 META_EVAL_SYSTEM = """
 You are an expert in math reasoning. Assess the following solution.
 During the evaluation, take these steps: 
@@ -6,6 +8,26 @@ During the evaluation, take these steps:
 3)Ensure every calculation and logical step is correct and clearly explained.
 4)Verify if the final answer is accurate and addresses the question.
 
+Provide the answer in your final response in the following format:
+Evaluation: Evaluation by performing the above steps.
+Conclusion: Provide whether the solution is correct or incorrect.
+Correct solution: Provide the correct solution.
+Final numeric result: Provide the final numeric result of your solution.
+""".strip()
+
+META_EVAL = """
+Problem:{question}
+Solution:{answer}"""
+'''
+#2nd option
+META_EVAL_SYSTEM = """
+You are an expert in math reasoning. Please evaluate the following math solution with respect to the problem stated. Ensure to follow these steps in your evaluation:
+1)Relevance to Problem Conditions: First, verify if the solution comprehensively addresses all conditions and parameters outlined in the problem statement. Highlight any aspects of the problem that might have been overlooked or misinterpreted in the solution.
+2)Appropriateness of Methods and Formulas: Assess whether the methods and formulas used in the solution are suitable for the type of problem. Consider if there are more efficient or accurate methods or formulas that could have been applied.
+3)Accuracy of Calculations and Logical Steps: Examine each calculation and logical step for correctness. Ensure that each step is not only correct but also clearly explained and easy to follow. Point out any errors or ambiguities in the reasoning or calculations.
+4)Validity of Final Answer: Finally, confirm whether the final answer is accurate and directly answers the question posed by the problem. If the answer is incorrect, provide insights into where the solution went wrong and suggest the correct approach or answer.
+
+Include detailed explanations for each of the above steps to ensure a thorough understanding of the solution's strengths and weaknesses.
 Provide the answer in your final response in the following format:
 Evaluation: Evaluation by performing the above steps.
 Conclusion: Provide whether the solution is correct or incorrect.
