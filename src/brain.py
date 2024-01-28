2d0068776a2bcaf0af5221817e2ec9c53f06e79a
Georgia256 <geormanifava@gmail.com>
2024-01-28 02:31:21 +0200

    first attempt

--- src/brain.py
+++ src/brain.py
@@ -667,7 +667,7 @@ class Brain:
         for i in range(max_steps):
             print("*****************NEW STEP*****************")
             print(f"The status array is {status}")
-            initial_promp = chat_input + [str(status)] + [output_string]
+            initial_promp = chat_input + str(status) + output_string
             #out = get_chat_response(self.args, initial_promp, self.api_key, self.ORG_ID)
             out = generate_text_phi(initial_promp,k)[0]
             outputs = parse_output_options(out)
