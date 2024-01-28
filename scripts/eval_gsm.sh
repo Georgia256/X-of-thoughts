90f4148228ce6422fc73a926ec6056002c640b1f
Georgia256 <geormanifava@gmail.com>
2024-01-28 19:26:38 +0200

    first attempt

--- scripts/eval_gsm.sh
+++ scripts/eval_gsm.sh
@@ -3,12 +3,12 @@ RANGE_END=$2
 TAG=$3
 
 python src/analyze.py \
-  --plan outputs/gsm/plan/${TAG}_plan_${RANGE_START}_${RANGE_END}.jsonl \
-  --cot outputs/gsm/cot/${TAG}_cot_${RANGE_START}_${RANGE_END}.jsonl \
-  --pot outputs/gsm/pot/${TAG}_pot_${RANGE_START}_${RANGE_END}.jsonl \
-  --eot outputs/gsm/eot/${TAG}_eot_${RANGE_START}_${RANGE_END}.jsonl \
+  #--plan outputs/gsm/plan/${TAG}_plan_${RANGE_START}_${RANGE_END}.jsonl \
+  #--cot outputs/gsm/cot/${TAG}_cot_${RANGE_START}_${RANGE_END}.jsonl \
+  #--pot outputs/gsm/pot/${TAG}_pot_${RANGE_START}_${RANGE_END}.jsonl \
+  #--eot outputs/gsm/eot/${TAG}_eot_${RANGE_START}_${RANGE_END}.jsonl \
   --tot outputs/gsm/eot/${TAG}_tot_${RANGE_START}_${RANGE_END}.jsonl \
-  --pot_assertion outputs/gsm/check_pot/${TAG}_check_pot_${RANGE_START}_${RANGE_END}.jsonl \
-  --eot_assertion outputs/gsm/check_eot/${TAG}_check_eot_${RANGE_START}_${RANGE_END}.jsonl \
-  --tag gsm_analysis
+ # --pot_assertion outputs/gsm/check_pot/${TAG}_check_pot_${RANGE_START}_${RANGE_END}.jsonl \
+ # --eot_assertion outputs/gsm/check_eot/${TAG}_check_eot_${RANGE_START}_${RANGE_END}.jsonl \
+ # --tag gsm_analysis
   #--metacognitive_eval outputs/gsm/metacognitive_eval_cot/${TAG}_v2_metacognitive_eval_${RANGE_START}_${RANGE_END}.jsonl \
