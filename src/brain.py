5176b16ea9f8c6b85eac0f473f4cab145131db50
Georgia256 <geormanifava@gmail.com>
2024-01-28 03:42:35 +0200

    first attempt

--- src/brain.py
+++ src/brain.py
@@ -618,7 +618,7 @@ class Brain:
 
         comp_prompt = self.build_chat_input_rank(comparison_prompt)
         #a = generate_text_phi(comp_prompt,1)
-        a=self.generate_text_phi(comp_prompt)[0]
+        a=self.generate_text_phi(comp_prompt)
         return a
     
     def reason_tot(self):
