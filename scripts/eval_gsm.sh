9d54bee4f178c628753631f077fe26851a2e9dc6
olgacode <olgabarlou@gmail.com>
2024-02-07 14:49:47 +0200

    Update eval_gsm.sh

--- scripts/eval_gsm.sh
+++ scripts/eval_gsm.sh
@@ -2,7 +2,7 @@ RANGE_START=$1
 RANGE_END=$2
 #TAG=$3
 
-python src/analyze.py \
+python src/analyze_test.py \
   --plan outputs/gsm/plan/deepseek_plan_rating_${RANGE_START}_${RANGE_END}.jsonl \
   --cot outputs/gsm/cot/phi_cot_${RANGE_START}_${RANGE_END}.jsonl \
   --pot outputs/gsm/pot/phi_pot_${RANGE_START}_${RANGE_END}.jsonl \
